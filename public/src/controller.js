b49cf76685fd150591d0df0867dd1fb9272fd8b9
Gennifer <gennifer.gates@gmail.com>
2015-01-18 19:53:58 -0800

    Add form and directives

--- public/src/controller.js
+++ public/src/controller.js
@@ -4,7 +4,7 @@ angular.module('ContactsApp')
 		$scope.fields = ['firstName', 'lastName'];
 
 		$scope.sort = function (field) {
-			$scope.sort.field = field
+			$scope.sort.field = field;
 			$scope.sort.order = !$scope.sort.order;
 		};
 
@@ -14,4 +14,24 @@ angular.module('ContactsApp')
 		$scope.show = function (id) {
 			$location.url('/contact/' + id);
 		}
+	})
+	.controller ('NewController', function ($scope, Contact, $location) {
+		$scope.contact = new Contact({
+		firstName: ['', 'text'], 
+		lastName: ['', 'text'],
+		email: ['', 'email'],
+		homePhone: ['', 'tel'],
+		cellPhone: ['', 'tel'],
+		birthday: ['', 'date'],
+		website: ['', 'url'],
+		address: ['', 'text']
+		});
+		$scope.save = function () {
+			if ($scope.newContact.$invalid) {
+				$scope.$broadcast('record:invalid');
+			} else {
+				$scope.contact.$save();
+				$location.url('/contacts');
+			}
+		};
 	});
